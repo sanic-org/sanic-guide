1b7de3e669e95a78084e5b696e4b64708ef8d308
Ryu JuHeon <saidbysolo@gmail.com>
2021-12-01 20:44:27 +0900

    choe(config): edit the config only the translated part

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -631,39 +631,52 @@ let ko_locales = {
   algolia: {},
   nav: [
     {
-      text: "홈",
+      text: "홈 (Home)",
       link: "/ko/",
     },
     // { text: "Announcements", link: "/announcements" },
     {
-      text: "문서",
+      text: "문서 (Docs)",
       items: [
         {
-          text: "유저 가이드",
+          text: "User Guide",
           items: [
             {
-              text: "일반",
+              text: "일반 (General)",
               link: "/ko/guide/getting-started.md",
             },
             {
-              text: "기초",
+              text: "기초 (Basics)",
               link: "/ko/guide/basics/app.md",
             },
             {
-              text: "고급",
-              link: "/ko/guide/advanced/class-based-views.md",
+              text: "Advanced",
+              link: "/en/guide/advanced/class-based-views.md",
             },
             {
-              text: "모범 사례",
-              link: "/ko/guide/best-practices/blueprints.md",
+              text: "Best Practices",
+              link: "/en/guide/best-practices/blueprints.md",
             },
             {
-              text: "실행 및 배포",
-              link: "/ko/guide/deployment/configuration.md",
+              text: "Running & Deploying",
+              link: "/en/guide/deployment/configuration.md",
             },
             {
-              text: "이거 어떻게 하나요...",
-              link: "/ko/guide/how-to/toc.md",
+              text: "How to...",
+              link: "/en/guide/how-to/toc.md",
+            },
+          ],
+        },
+        {
+          text: "공식 플러그인들",
+          items: [
+            {
+              text: "Sanic Extensions",
+              link: "/en/plugins/sanic-ext/getting-started.md",
+            },
+            {
+              text: "Sanic Testing",
+              link: "/en/plugins/sanic-testing/testing.md",
             },
           ],
         },
@@ -677,15 +690,15 @@ let ko_locales = {
           ],
         },
         {
-          text: "조직 문서",
+          text: "Org 문서",
           items: [
             {
               text: "정책",
-              link: "/en/guide/project/policies.md",
+              link: "/en/org/policies.md",
             },
             {
               text: "S.C.O.P.E.",
-              link: "/en/guide/project/scope.md",
+              link: "/en/org/scope.md",
             },
           ],
         },
@@ -716,12 +729,12 @@ let ko_locales = {
   sidebar: {
     "/ko/guide/": [
       {
-        title: "일반",
+        title: "일반 (General)",
         sidebarDepth: 1,
         children: ["/ko/guide/", "/ko/guide/getting-started.md"],
       },
       {
-        title: "기초",
+        title: "기초 (Basics)",
         sidebarDepth: 1,
         children: [
           "/ko/guide/basics/app.md",
@@ -737,73 +750,168 @@ let ko_locales = {
         ],
       },
       {
-        title: "고급",
+        title: "Advanced",
         sidebarDepth: 1,
         children: [
-          "/ko/guide/advanced/class-based-views.md",
-          "/ko/guide/advanced/proxy-headers.md",
-          "/ko/guide/advanced/streaming.md",
-          "/ko/guide/advanced/websockets.md",
-          "/ko/guide/advanced/versioning.md",
-          "/ko/guide/advanced/signals.md",
+          "/en/guide/advanced/class-based-views.md",
+          "/en/guide/advanced/proxy-headers.md",
+          "/en/guide/advanced/streaming.md",
+          "/en/guide/advanced/websockets.md",
+          "/en/guide/advanced/versioning.md",
+          "/en/guide/advanced/signals.md",
         ],
       },
       {
-        title: "모범 사례",
+        title: "Best Practices",
         sidebarDepth: 1,
         children: [
-          "/ko/guide/best-practices/blueprints.md",
-          "/ko/guide/best-practices/exceptions.md",
-          "/ko/guide/best-practices/decorators.md",
-          "/ko/guide/best-practices/logging.md",
-          "/ko/guide/best-practices/testing.md",
+          "/en/guide/best-practices/blueprints.md",
+          "/en/guide/best-practices/exceptions.md",
+          "/en/guide/best-practices/decorators.md",
+          "/en/guide/best-practices/logging.md",
+          "/en/guide/best-practices/testing.md",
         ],
       },
       {
-        title: "실행 및 배포",
+        title: "Running & Deploying",
         sidebarDepth: 2,
         children: [
-          "/ko/guide/deployment/configuration.md",
-          "/ko/guide/deployment/development.md",
-          "/ko/guide/deployment/running.md",
+          "/en/guide/deployment/configuration.md",
+          "/en/guide/deployment/development.md",
+          "/en/guide/deployment/running.md",
           // "/guide/deployment/server-choice.md",
-          "/ko/guide/deployment/nginx.md",
+          "/en/guide/deployment/nginx.md",
           // "/guide/deployment/docker.md",
           // "/guide/deployment/kubernetes.md",
         ],
       },
       {
-        title: "이거 어떻게 하나요...",
+        title: "How to...",
         sidebarDepth: 1,
         children: [
-          "/ko/guide/how-to/toc.md",
-          "/ko/guide/how-to/mounting.md",
-          "/ko/guide/how-to/authentication.md",
-          "/ko/guide/how-to/autodiscovery.md",
-          "/ko/guide/how-to/cors.md",
+          "/en/guide/how-to/toc.md",
+          "/en/guide/how-to/mounting.md",
+          "/en/guide/how-to/authentication.md",
+          "/en/guide/how-to/autodiscovery.md",
+          "/en/guide/how-to/cors.md",
           // "/guide/how-to/db.md",
           // "/guide/how-to/decorators.md",
           // "/guide/how-to/validation.md",
           // "/guide/how-to/csrf.md",
           // "/guide/how-to/serialization.md",
+          // "/en/guide/how-to/sqlalchemy.md",
+          "/en/guide/how-to/orm.md",
+          "/en/guide/how-to/static-redirects.md",
           // "/guide/how-to/task-queue.md",
-          "/ko/guide/how-to/tls.md",
+          "/en/guide/how-to/tls.md",
           // "/guide/how-to/websocket-feed.md",
           // "/guide/how-to/server-sent-events.md",
         ],
       },
       {
-        title: "릴리즈 노트",
-        sidebarDepth: 1,
-        children: ["/ko/guide/release-notes/v21.3.md"],
-      },
-      {
-        title: "조직",
+        title: "Release Notes",
         sidebarDepth: 1,
         children: [
-          "/ko/guide/project/policies.md",
-          "/ko/guide/project/scope.md",
+          "/en/guide/release-notes/v21.9.md",
+          "/en/guide/release-notes/v21.6.md",
+          "/en/guide/release-notes/v21.3.md",
         ],
+      },
+      {
+        title: "Plugins",
+        sidebarDepth: 1,
+        children: [
+          ["/en/plugins/sanic-ext/getting-started.md", "Sanic Extensions"],
+          ["/en/plugins/sanic-testing/testing.md", "Sanic Testing"],
+        ],
+      },
+      {
+        title: "Organization",
+        sidebarDepth: 1,
+        children: ["/en/org/policies.md", "/en/org/scope.md"],
+      },
+    ],
+
+    "/en/plugins/": [
+      {
+        title: "User Guide",
+        sidebarDepth: 1,
+        children: [
+          ["/en/guide/", "General"],
+          ["/en/guide/basics/app.md", "Basics"],
+          ["/en/guide/advanced/class-based-views.md", "Advanced"],
+          ["/en/guide/best-practices/blueprints.md", "Best Practices"],
+          ["/en/guide/deployment/configuration.md", "Running & Deploying"],
+          ["/en/guide/how-to/toc.md", "How to..."],
+        ],
+      },
+      {
+        title: "Sanic Extensions",
+        sidebarDepth: 1,
+        children: [
+          "/en/plugins/sanic-ext/getting-started.md",
+          {
+            title: "HTTP goodies",
+            children: [
+              "/en/plugins/sanic-ext/http/methods.md",
+              "/en/plugins/sanic-ext/http/cors.md",
+            ],
+          },
+          "/en/plugins/sanic-ext/convenience.md",
+          "/en/plugins/sanic-ext/injection.md",
+          {
+            title: "OpenAPI",
+            sidebarDepth: 2,
+            children: [
+              "/en/plugins/sanic-ext/openapi/basic.md",
+              "/en/plugins/sanic-ext/openapi/ui.md",
+              "/en/plugins/sanic-ext/openapi/decorators.md",
+              "/en/plugins/sanic-ext/openapi/advanced.md",
+              "/en/plugins/sanic-ext/openapi/autodoc.md",
+            ],
+          },
+          "/en/plugins/sanic-ext/validation.md",
+          "/en/plugins/sanic-ext/configuration.md",
+        ],
+      },
+      {
+        title: "Sanic Testing",
+        sidebarDepth: 1,
+        children: ["/en/plugins/sanic-testing/testing.md"],
+      },
+      {
+        title: "Organization",
+        sidebarDepth: 1,
+        children: ["/en/org/policies.md", "/en/org/scope.md"],
+      },
+    ],
+
+    "/en/org/": [
+      {
+        title: "User Guide",
+        sidebarDepth: 1,
+        children: [
+          ["/en/guide/", "General"],
+          ["/en/guide/basics/app.md", "Basics"],
+          ["/en/guide/advanced/class-based-views.md", "Advanced"],
+          ["/en/guide/best-practices/blueprints.md", "Best Practices"],
+          ["/en/guide/deployment/configuration.md", "Running & Deploying"],
+          ["/en/guide/how-to/toc.md", "How to..."],
+        ],
+      },
+      {
+        title: "Plugins",
+        sidebarDepth: 1,
+        children: [
+          ["/en/plugins/sanic-ext/getting-started.md", "Sanic Extensions"],
+          ["/en/plugins/sanic-testing/testing.md", "Sanic Testing"],
+        ],
+      },
+      {
+        title: "Organization",
+        sidebarDepth: 1,
+        collapsable: false,
+        children: ["/en/org/policies.md", "/en/org/scope.md"],
       },
     ],
   },
