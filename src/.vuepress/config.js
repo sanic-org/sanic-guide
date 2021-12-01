797ef31fbb6352df63f363272ee1804b5b8b1dd9
tnt <45692264+tuna2134@users.noreply.github.com>
2021-12-01 18:33:05 +0900

    Little edit

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -21,6 +21,13 @@ let zh = {
     "Sanic 是 Python 3.7+ Web 服务器和 Web 框架，旨在提高性能。它允许使用 Python 3.5 中添加的 `async`/`await`等待语法，这使得您的代码有效的避免阻塞从而达到提升响应速度的目的。",
 };
 
+let ja = {
+  lang: "ja-JA",
+  title: "Sanic フレームワーク",
+  description:
+    "SanicはPython3.7以上で書かれており、処理速度が速いWebサーバーとウェブフレームワークです。Python 3.5で追加された非同期/待機構文の使用が可能になり、コードがノンブロッキングでスピーディーになります。",
+};
+
 /*
 ----------------------------------------------------------------------------------------------------------------
     please add language detailed config here, after that, please add it to site_config.themeConfig.locales .
