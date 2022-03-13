3486f7d065ecf3714d2d05bb6d5a8cd9b70a12ab
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-13 18:54:52 +0900

    Create locale_detail_ja.js

---
+++ src/.vuepress/locales/details/locale_detail_ja.js
@@ -0,0 +1,11 @@
+const sidebar_ja = require("./sidebar/sidebar_ja");
+const nav_ja = require("./nav/nav_ja");
+
+let ja_locales = {
+   selectText: "Languages",
+   label: "Japanese",
+   editLinkText: "Edit this page on GitHub",
+   algolia: {},
+   nav: nav_ja,
+   sidebar: sidebar_ja,
+};
