d3bd10d286848cc8e9553c2023d76427259f55a6
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-13 18:51:56 +0900

    Create locale_abstract_ja.js

---
+++ src/.vuepress/locales/abstract/locale_abstract_ja.js
@@ -0,0 +1,7 @@
+module.exports = {
+   lang: "ja-JA",
+   title: "Sanic フレームワーク",
+   current: "現在のバージョン:21.12",
+   description:
+     "SanicはPython3.7以上で書かれており、処理速度が速いWebサーバーとウェブフレームワークです。Python 3.5で追加された非同期/待機構文の使用が可能になり、コードがノンブロッキングでスピーディーになります。",
+};
