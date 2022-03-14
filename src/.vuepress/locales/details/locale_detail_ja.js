f88ad63a6568d2c797b255fd53ba72d7d8e43849
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-03-14 14:46:28 +0900

    forgot a module.exports

--- src/.vuepress/locales/details/locale_detail_ja.js
+++ src/.vuepress/locales/details/locale_detail_ja.js
@@ -1,7 +1,7 @@
 const sidebar_ja = require("./sidebar/sidebar_ja");
 const nav_ja = require("./nav/nav_ja");
 
-let ja_locales = {
+module.exports = ja_locales = {
    selectText: "Languages",
    label: "Japanese",
    editLinkText: "Edit this page on GitHub",
