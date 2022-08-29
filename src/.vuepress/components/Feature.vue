7112ae59b821c54c81740ca9d971b283e74c7f69
Adam Hopkins <adam@amhopkins.com>
2022-08-29 12:20:14 +0300

    Add feature requests page (#129)

---
+++ src/.vuepress/components/Feature.vue
@@ -0,0 +1,40 @@
+<script setup>
+const props = defineProps({
+  issue: Object,
+});
+const emojis = {
+  "+1": "👍",
+  "-1": "👎",
+  confused: "😕",
+  eyes: "👀",
+  heart: "❤️",
+  hooray: "🎉",
+  laugh: "😆",
+  rocket: "🚀",
+};
+</script>
+<template>
+  <div>
+    <h3>
+      <a
+        :href="`https://github.com/sanic-org/sanic/issues/${issue.number}`"
+        target="_blank"
+      >
+        #{{ issue.number }}
+      </a>
+      {{ issue.title }}
+    </h3>
+    <div class="reactions">
+      <span v-for="(item, key, index) in issue.reactions" :key="index">
+        <template v-if="emojis[key] && item > 0">
+          {{ emojis[key] }} {{ item }}
+        </template>
+      </span>
+    </div>
+  </div>
+</template>
+<style scoped>
+h3 {
+  margin-bottom: 0.5rem;
+}
+</style>
---
+++ src/en/org/feature_requests.md
@@ -0,0 +1,5 @@
+# Feature Requests
+
+To vote on a feature request, visit the [GitHub Issues](https://github.com/sanic-org/sanic/issues?q=is%3Aissue+is%3Aopen+label%3A%22feature+request%22%2CRFC+sort%3Areactions-%2B1-desc) and add a reaction
+
+<FeatureRequests />
