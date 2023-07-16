9fb6ad1e650e84dadfa8301c94dd99ac5b4eead7
Adam Hopkins <adam@amhopkins.com>
2023-07-16 16:18:18 +0300

    Prepare for v23.6 release

--- src/.vuepress/locales/details/nav/nav_en.js
+++ src/.vuepress/locales/details/nav/nav_en.js
@@ -1,105 +1,105 @@
 module.exports = [
-  {
-    text: "Home",
-    link: "/en/",
-  },
-  // { text: "Announcements", link: "/announcements" },
-  {
-    text: "Docs",
-    items: [
-      {
-        text: "User Guide",
+    {
+        text: "Home",
+        link: "/en/",
+    },
+    // { text: "Announcements", link: "/announcements" },
+    {
+        text: "Docs",
         items: [
-          {
-            text: "General",
-            link: "/en/guide/getting-started.md",
-          },
-          {
-            text: "Basics",
-            link: "/en/guide/basics/app.md",
-          },
-          {
-            text: "Advanced",
-            link: "/en/guide/advanced/class-based-views.md",
-          },
-          {
-            text: "Best Practices",
-            link: "/en/guide/best-practices/blueprints.md",
-          },
-          {
-            text: "Running & Deploying",
-            link: "/en/guide/deployment/configuration.md",
-          },
-          {
-            text: "How to...",
-            link: "/en/guide/how-to/toc.md",
-          },
-          {
-            text: "Latest Release Notes",
-            link: "/en/guide/release-notes/v23.3.md",
-          },
+            {
+                text: "User Guide",
+                items: [
+                    {
+                        text: "General",
+                        link: "/en/guide/getting-started.md",
+                    },
+                    {
+                        text: "Basics",
+                        link: "/en/guide/basics/app.md",
+                    },
+                    {
+                        text: "Advanced",
+                        link: "/en/guide/advanced/class-based-views.md",
+                    },
+                    {
+                        text: "Best Practices",
+                        link: "/en/guide/best-practices/blueprints.md",
+                    },
+                    {
+                        text: "Running & Deploying",
+                        link: "/en/guide/deployment/configuration.md",
+                    },
+                    {
+                        text: "How to...",
+                        link: "/en/guide/how-to/toc.md",
+                    },
+                    {
+                        text: "Latest Release Notes",
+                        link: "/en/guide/release-notes/v23.6.md",
+                    },
+                ],
+            },
+            {
+                text: "Official Plugins",
+                items: [
+                    {
+                        text: "Sanic Extensions",
+                        link: "/en/plugins/sanic-ext/getting-started.md",
+                    },
+                    {
+                        text: "Sanic Testing",
+                        link: "/en/plugins/sanic-testing/getting-started.md",
+                    },
+                ],
+            },
+            {
+                text: "API Docs",
+                items: [
+                    {
+                        text: "View API Docs",
+                        link: "https://sanic.readthedocs.io",
+                    },
+                ],
+            },
+            {
+                text: "Org Docs",
+                items: [
+                    {
+                        text: "Feature Requests",
+                        link: "/en/org/feature_requests.md",
+                    },
+                    {
+                        text: "Policies",
+                        link: "/en/org/policies.md",
+                    },
+                    {
+                        text: "S.C.O.P.E.",
+                        link: "/en/org/scope.md",
+                    },
+                ],
+            },
         ],
-      },
-      {
-        text: "Official Plugins",
+    },
+    {
+        text: "Community",
         items: [
-          {
-            text: "Sanic Extensions",
-            link: "/en/plugins/sanic-ext/getting-started.md",
-          },
-          {
-            text: "Sanic Testing",
-            link: "/en/plugins/sanic-testing/getting-started.md",
-          },
+            {
+                text: "Forums",
+                link: "https://community.sanicframework.org/",
+            },
+            {
+                text: "Discord",
+                link: "https://discord.gg/FARQzAEMAA",
+            },
+            {
+                text: "Twitter",
+                link: "https://twitter.com/sanicframework",
+            },
+            {
+                text: "Sponsorship",
+                link: "https://opencollective.com/sanic-org/",
+            },
         ],
-      },
-      {
-        text: "API Docs",
-        items: [
-          {
-            text: "View API Docs",
-            link: "https://sanic.readthedocs.io",
-          },
-        ],
-      },
-      {
-        text: "Org Docs",
-        items: [
-          {
-            text: "Feature Requests",
-            link: "/en/org/feature_requests.md",
-          },
-          {
-            text: "Policies",
-            link: "/en/org/policies.md",
-          },
-          {
-            text: "S.C.O.P.E.",
-            link: "/en/org/scope.md",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    text: "Community",
-    items: [
-      {
-        text: "Forums",
-        link: "https://community.sanicframework.org/",
-      },
-      {
-        text: "Discord",
-        link: "https://discord.gg/FARQzAEMAA",
-      },
-      {
-        text: "Twitter",
-        link: "https://twitter.com/sanicframework",
-      },
-      {
-        text: "Sponsorship",
-        link: "https://opencollective.com/sanic-org/",
-      },
-    ],
-  },
+    },
 ];
